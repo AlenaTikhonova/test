4864bb6ef6b909d40cad6288202877add5fb8af2
AlenaTikhonova <pntra@mail.ru>
2015-02-22 19:28:07 +0300

    все классы в моделях

--- view/index.php
+++ view/index.php
@@ -1,7 +1,39 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Oleg
- * Date: 19.02.15
- * Time: 17:19
- */ 
+class Article {
+    public $time;
+    public $text;
+    public function DateTranslate($time){
+
+
+        return date( 'H\hi l d F', $time );
+    }
+    public function view(){
+        echo $this->text;
+    }
+
+
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+
+
+
+<?php foreach ($articles as  $article):?></body><br>
+
+<p><a href = page.php?id=<?php echo $article['id']?>>
+        <?php $in = $article['time'];
+        $a= new Article();
+        echo $a->DateTranslate($in);?><br/>
+        <?php echo $article['title'];?></a></p>
+
+<?php endforeach ?>
+
+<a href="../add.php">add new</a>
+</body>
+</html>
